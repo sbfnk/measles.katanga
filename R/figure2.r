e513f4a0e1d669245cdc2f93b8d0f6ec3de1c41a
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2019-11-08 15:20:52 +0000

    pre-submission figure updates

--- R/figure2.r
+++ R/figure2.r
@@ -5,26 +5,28 @@
 ##' @importFrom ggplot2 ggplot aes facet_wrap geom_bar scale_y_continuous theme element_text scale_fill_brewer scale_color_brewer scale_x_datetime
 ##' @importFrom scales comma
 ##' @importFrom cowplot theme_cowplot
+##' @importFrom lubridate floor_date
 ##' @author Sebastian Funk
 ##' @export
 figure2 <- function()
 {
     cases_age <- katanga_measles_cases %>%
-        group_by(week_start, province, age) %>%
+        mutate(month_start=floor_date(week_start, "months")) %>%
+        group_by(month_start, age) %>%
         summarise(cases=sum(cases, na.rm=TRUE),
                   deaths=sum(deaths, na.rm=TRUE))
 
     cases_age_sum <- cases_age %>%
-        filter(week_start >= "2010-01-01" & week_start <  "2016-01-01") %>%
+        filter(month_start >= "2010-01-01" & month_start <  "2016-01-01") %>%
         mutate(age=factor(age, levels=c("1-4", "5+"))) %>%
-        group_by(week_start, province, age) %>%
+        group_by(month_start, age) %>%
         summarise(cases=sum(cases, na.rm=TRUE)) %>%
-        ungroup
+        ungroup %>%
+        arrange(desc(age))
 
-    p <- ggplot(cases_age_sum, aes(x=week_start, y=cases, color=age, fill=age)) +
-        facet_wrap(~province) +
+    p <- ggplot(cases_age_sum, aes(x=month_start, y=cases, color=age, fill=age)) +
         geom_bar(stat="identity", position="stack") +
-        scale_y_continuous("Weekly incidence", labels=comma)+
+        scale_y_continuous("Monthly incidence", labels=comma)+
         theme_cowplot() +
         theme(axis.text.x=element_text(angle=45, hjust=1), legend.position="top") +
         scale_fill_brewer("Age", palette="Dark2") +
--- R/figure3.r
+++ R/figure3.r
@@ -19,24 +19,24 @@ figure3 <- function()
         complete(health_zone, year) %>%
         replace_na(list(cases=0)) %>%
         spread(year, cases) %>%
-        mutate(`201113`=`2011`+`2012`+`2013`) %>%
+        mutate(`201013`=`2010`+`2011`+`2012`+`2013`) %>%
         gather(year, cases, 2:ncol(.)) %>%
         left_join(populations, by="health_zone") %>%
         mutate(incidence=cases/population) %>%
         select(-cases, -population, -l5pop) %>%
         spread(year, incidence) %>%
-        mutate(size=if_else(`201113`>0.015, "previous_big", "smallish"),
+        mutate(size=if_else(`201013`>0.015, "previous_big", "smallish"),
            size=if_else(`2015`>0.005, "later_big", size),
            size=factor(size, levels=c("later_big", "previous_big", "smallish")),
            label_health_zone=if_else(size == "smallish", "", health_zone))
 
-    p <- ggplot(cases_annual, aes(x=`201113`*1000, y=`2015`*1000,
+    p <- ggplot(cases_annual, aes(x=`201013`*1000, y=`2015`*1000,
                                   label=label_health_zone))+
         geom_label_repel(label.padding = 0.15, label.size=NA,
                          segment.color="darkgrey") +
         geom_point(aes(color=size)) +
         scale_color_manual(values=c("black", "red", "darkgrey")) +
-        scale_x_continuous("Incidence in 2011-13 (per 1000)") +
+        scale_x_continuous("Incidence in 2010-13 (per 1000)") +
         scale_y_continuous("Incidence in 2015 (per 1000)") +
         expand_limits(y=-1) +
         geom_hline(yintercept=0, linetype="dashed")+
